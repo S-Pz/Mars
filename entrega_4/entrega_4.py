3e67be91fa61f60598c5d0ffc454afb596d697dc
S-Pz <savio_fr@outlook.com>
2025-01-30 16:08:32 -0300

    Changing gitignore file and data_filter

--- entrega_4/entrega_4.py
+++ entrega_4/entrega_4.py
@@ -23,23 +23,19 @@ with open("posts_data.json", "r") as file:
 
 stop_words = set(stopwords.words('english'))
 
-key_words = [ 'canada', 'facebook', 'instagram',
-              'eua', 'sa','Elon Musk', 'joebiden','Kamala Harris',
-              'Lindsey Graham','greenland','island','acquire',
-              'purchase','buy','military','control','markzuckerberg',
-              'zuckerberg','trump','Obama'
+key_words = [ 'canada', 'facebook', 'instagram','eua', 'usa','Elon Musk', 'joebiden','Kamala Harris',
+              'Lindsey Graham','greenland','island','acquire','purchase','buy','military','control',
+              'markzuckerberg','zuckerberg','trump','Obama'
             ]
 
 negative_words = [
-    "corrupt", "unfair", "biased", "lies", "dishonest", "scam", "fraud", 
-    "hate", "terrible", "awful", "disgusting", "nonsense", "rigged", 
-    "manipulated", "fake", "wrong", "unacceptable", "shameful", "problematic", 
-    "disgrace", "toxic", "incompetent", "hypocrite", "crooked", "failing", 
-    "horrible", "evil", "selfish", "ruined", "destroyed", "overrated", 
-    "controversial", "dangerous", "threat", "misleading", "cheated", 
-    "broken", "backward", "divisive", "outdated", "pathetic", "ignorant", 
-    "arrogant", "oppressive", "exploitative", "despicable", "racist","crazy",
-    'fat','ugly','stupid','dumb','idiot','fool','foolish','dull','dullard',
+    "corrupt", "unfair", "biased", "lies", "dishonest", "scam", "fraud","hate", "terrible", "awful",
+    "disgusting", "nonsense", "rigged","manipulated", "fake", "wrong", "unacceptable", "shameful", 
+    "problematic","disgrace", "toxic", "incompetent", "hypocrite", "crooked", "failing","horrible",
+    "evil","selfish", "ruined", "destroyed", "overrated", "controversial", "dangerous", "threat",
+    "misleading", "cheated","broken", "backward", "divisive", "outdated", "pathetic", "ignorant", 
+    "arrogant", "oppressive", "exploitative", "despicable", "racist","crazy",'fat','ugly','stupid',
+    'dumb','idiot','fool','foolish','dull','dullard',
 ]
 
 filtred_sentences = []
@@ -55,8 +51,9 @@ for entry in dataset:
     if filtered_words: 
         filtred_sentences.append(filtered_words)
 
-####################### Word2Vec #####################################################
-model = Word2Vec(filtred_sentences, vector_size=100, window=5, min_count=5, workers=4)
+print(filtred_sentences)
+#################################### Word2Vec #####################################################
+model = Word2Vec(filtred_sentences, vector_size=100, window=10, min_count=1, workers=4, sg=1)
 
 valid_key_words = [word for word in key_words if word in model.wv]
 valid_negative_words = [word for word in negative_words if word in model.wv]
@@ -65,12 +62,15 @@ if valid_key_words:
     similar_words = model.wv.most_similar(positive=valid_key_words, negative=valid_negative_words)
     relation = [(item[0], round(item[1], 2)) for item in similar_words]
     print (relation)
+    #colocar um para relacionar com todos tipo o item 0 com o 1,2,3,4,5,6,7,8,9 e assim por diante
     print("\n🔹 Palavras mais associadas às Key Words:")
     for word, score in relation:
         print(f"{word}: {score}")
 
-    print("\n📊 🔥 Análise de Sentimento das Palavras Relacionadas:")
     for word, score in relation:
-        sentiment_score = analyzer.polarity_scores(word)['compound']
+        related_sentences = [entry['comments'] for entry in dataset if word in entry['comments']]
+        combined_text = " ".join(related_sentences)
+        sentiment_score = analyzer.polarity_scores(combined_text)['compound']
         sentiment = "Positivo" if sentiment_score > 0 else "Negativo" if sentiment_score < 0 else "Neutro"
+        
         print(f"Palavra: {word} - Score: {sentiment_score} ({sentiment})")
